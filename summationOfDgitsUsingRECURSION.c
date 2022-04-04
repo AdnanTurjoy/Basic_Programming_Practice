107b60f4153577ae433d609ae2cbe55900a7f1ce
AdnanTurjoy <aturjoy4@gmail.com>
2022-04-04 16:57:29 +0600

    recursion

--- summationOfDgitsUsingRECURSION.c
+++ summationOfDgitsUsingRECURSION.c
@@ -1,1 +1,21 @@
 
+
+#include<stdio.h>
+int sumOfDigit(int n){
+if(n==0){
+    return 0;
+}
+else{
+        return n%10 + sumOfDigit(n/10);
+
+}
+}
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int sum = sumOfDigit(n);
+    printf("%d",sum);
+    return 0;
+}
