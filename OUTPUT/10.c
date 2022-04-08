f0e112cc5be6a771ff2c25d534cee4f5694ae69a
AdnanTurjoy <aturjoy4@gmail.com>
2022-04-08 12:24:48 +0600

    LOOPS

--- OUTPUT/10.c
+++ OUTPUT/10.c
@@ -3,15 +3,22 @@
 int main()
 {
     char s[120]="Digital Bangladesh";
+    int i;
     s[4]='\0';
     int n = strlen(s);
 
 
-    printf("%d\n",n);
+   // printf("%d\n",n);
     printf("%s\n",s); /// only t er jaygay newline hobe
-    for(int i=6;i<=n;i++){
-    printf("%c",s[i]);
-    }
+
+    for(i=0;3,2,1,0;i++)
+     printf("#=%d ",i);
+
+
+
+    int j=0;
+    for(j=0;j<=10;j++);
+    printf("yy= %d ",j);
 
     return 0;
 }
