70e83093da1a0041d3f47d2d87ce38093403ff0f
Adnan Turjoy <aturjoy4@gmail.com>
2022-08-27 03:18:21 +0600

    Update POINTERRRR.c

--- OUTPUT/POINTERRRR.c
+++ OUTPUT/POINTERRRR.c
@@ -1,20 +1,18 @@
-
 #include<stdio.h>
 int main()
 {
     int a[5]= {3,2,5,4,8};
     int *pt;
-    pt=&a;
+     pt=a;
 
    /* for(int i=0; i<5; i++)
     {
         printf("%d",*pt);
         *pt++;
-
     }*/
 
 
-    printf("\n%d",*(pt+3));
+    printf("\n%d",*(pt+2));
 
     return 0;
 }
