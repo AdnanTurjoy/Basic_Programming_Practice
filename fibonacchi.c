78c1c5a5119da3f9f4087b2419c9e9597b518a40
Adnan Turjoy <aturjoy4@gmail.com>
2022-08-27 03:14:28 +0600

    Update fibonacchi.c

--- fibonacchi.c
+++ fibonacchi.c
@@ -1,8 +1,10 @@
-/// nth position  fibonacchi
 #include<stdio.h>
 int fibo(int n){
- if(n<=1){
-    return n;
+ if(n==1){
+    return 1;
+ }
+ else if(n==0){
+     return 0;
  }
  else{
     return fibo(n-1) + fibo(n-2);
